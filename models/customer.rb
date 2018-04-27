70a97e15f06eeb2fc734ec2ac265eb906d9d41cf
Euan <bell_designs@outlook.com>
2018-04-27 14:37:42 +0100

    completed CRUD

---
+++ models/customer.rb
@@ -0,0 +1,50 @@
+require_relative('../db/sql_runner.rb')
+
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :funds, :name
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers(
+    name, funds)
+    VALUES ($1, $2)
+    RETURNING id"
+    values = [@name, @funds]
+    customer = SqlRunner.run( sql, values ).first
+    @id = customer['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    customer_data = SqlRunner.run(sql)
+    return Film.map_items(customer_data)
+  end
+
+  def self.map_items(customer_data)
+     result = customer_data.map { |customer| Customer.new(customer)}
+     return result
+   end
+
+   def update()
+      sql = "UPDATE customers SET (name, funds) = ($1, $2) WHERE id = $3"
+      values = [@name, @funds, @id]
+      SqlRunner.run(sql, values)
+    end
+
+    def self.delete_all()
+   sql = "DELETE FROM customers"
+   SqlRunner.run(sql)
+  end
+
+
+
+
+end
