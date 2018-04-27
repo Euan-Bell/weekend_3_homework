588f924d8f220b2527efb8b664e77cd047b1195d
Euan <bell_designs@outlook.com>
2018-04-27 13:11:10 +0100

    added self.all method

--- db/console.rb
+++ db/console.rb
@@ -5,33 +5,47 @@ require_relative( '../models/ticket' )
 require('pry')
 
 customer1 = Customer.new({ 'name' => 'Paul',
-  'funds' => '20'})
+'funds' => '20'})
 customer1.save()
 
+
 customer2 = Customer.new({ 'name' => 'Euan',
-  'funds' => '15'})
+'funds' => '15'})
 customer2.save()
 
 customer3 = Customer.new({ 'name' => 'Joe',
-  'funds' => '30'})
+'funds' => '30'})
 customer3.save()
 
 
-film1 = Film.new({ 'title' => 'IT',
-    'price' => '10'})
+film1 = Film.new({
+'title' => 'IT',
+'price' => '10'})
+film1.save()
 
-film2 = Film.new({ 'title' => 'The Shinning',
-    'price' => '10'})
 
-film3 = Film.new({ 'title' => 'Kujo',
-    'price' => '10'})
+
+film2 = Film.new({
+'title' => 'The Shinning',
+'price' => '10'})
+film2.save()
+
+
+film3 = Film.new({
+'title' => 'Kujo',
+'price' => '10'})
+film3.save()
+
 
 
 ticket1 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket1.save()
 
-ticket1 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket2 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket2.save()
 
 ticket3 = Ticket.new({ 'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket3.save()
 
 
 
